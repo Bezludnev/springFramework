37cab0702609ed01bfe41637f1e3a095dababf88
dizzy <bezludnev@gmail.com>
2020-08-02 23:46:28 +0300

    работает

--- src/main/java/ru/otus/spring01/Main.java
+++ src/main/java/ru/otus/spring01/Main.java
@@ -1,5 +1,6 @@
 package ru.otus.spring01;
 
+import com.sun.org.apache.bcel.internal.util.ClassPath;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.spring01.domain.Person;
 import ru.otus.spring01.service.PersonService;
@@ -7,7 +8,9 @@ import ru.otus.spring01.service.PersonService;
 public class Main {
 
     public static void main(String[] args) {
-        Person ivan = null;
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        PersonService service = context.getBean(PersonService.class);
+        Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
     }
 }
